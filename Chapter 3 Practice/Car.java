6ad8d7a4a2d6c544b5c68868643b238f33bd2778
jhperales <jhperales@NN123-HA-18.HS.sd203.org>
2015-09-11 14:23:14 -0500

    chapter 3 practice
    beginning classes

---
+++ Chapter 3 Practice/Car.java
@@ -0,0 +1,40 @@
+
+
+/**
+ * a representation of an automobile.
+ * 
+ * @author Jerome
+ * @version 9-10-15 (Alpha v 1.0)
+ */
+public class Car
+{
+    /** specifies the fuel efficiency of the car in units of miles per gallon (mpg) ) */
+    private double fuelIntTank;
+
+    /**
+     * Default constructor for objects of class Car
+     */
+    public Car()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *    that describes the operation of the method
+     *
+     * @pre        preconditions for the method
+     *            (what the method assumes about the method's parameters and class's state)
+     * @post    postconditions for the method
+     *            (what the method guarantees upon completion)
+     * @param    y    description of parameter y
+     * @return    description of the return value
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x+y;
+    }
+
+}
