697d629246730dc03ebd315af1d1ce5555719ca8
jhperales <jhperales@stu.naperville203.org>
2015-09-17 23:06:30 -0500

    started vending machine class

---
+++ Chapter 3 Practice/VendingMachine.java
@@ -0,0 +1,22 @@
+
+
+/**
+ * Resembles a real-life vending machine
+ * 
+ * @author Jerome 
+ * @version Alpha 1.0
+ */
+public class VendingMachine
+{
+    /** specifies number of cans in machine and tokens collected */
+    private int tokens;
+    private int cans;
+    /**
+     * Default constructor for objects of class VendingMachine
+     */
+    public VendingMachine()
+    {
+        tokens = 0;
+        cans = 100;
+    }
+}
