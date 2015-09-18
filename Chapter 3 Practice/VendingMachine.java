528880e5b488e18f5061a34d1b76380b10746285
jhperales <jhperales@stu.naperville203.org>
2015-09-17 23:55:09 -0500

    update
    added insertToken and fillUp method to class VendingMachine, created
    test class for class VendingMachine

--- Chapter 3 Practice/VendingMachine.java
+++ Chapter 3 Practice/VendingMachine.java
@@ -19,4 +19,33 @@ public class VendingMachine
         tokens = 0;
         cans = 100;
     }
+    
+    /**
+     * acts as a mechinism to accept tokens
+     *
+     * @pre     token count cannot be negative int
+     * @post    token count goes up 1
+     * 
+     *
+     */
+    public void insertToken()
+    {
+        // Instructions for updating token and can counts
+        this.tokens += 1;
+        this.cans -= 1;
+    }
+    
+    /**
+     * resembles restocking machine with cans
+     *
+     * @post    can count updated
+     * @param   specified int value of number of cans to add
+     * 
+     */
+    public void fillUp(int cans)
+    {
+        // adds cans
+        this.cans += cans;
+    }
+
 }
