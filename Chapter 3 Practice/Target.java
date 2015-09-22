93424e0e8379f5c99f8cf2ea1d0dbfe358a31def
jhperales <jhperales@NN123-HA-18.HS.sd203.org>
2015-09-22 15:06:28 -0500

    update

--- Chapter 3 Practice/Target.java
+++ Chapter 3 Practice/Target.java
@@ -4,3 +4,36 @@ import java.awt.geom.Ellipse2D;
 import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
 
+public class Target
+{
+    private int xLeft;
+    private int yTop;
+    
+    /**
+     * a target that can be placed anywhere
+     */
+    public Target(int x, int y)
+    {
+        /**
+         * Contructs a target with a given top left corner
+         * @param x the x-cooridnate of the top left corner
+         * @param y the y-coordinate of the top left corner
+         */
+        
+        xLeft = x;
+        yTop = y;
+    }
+    
+    public void draw_black(Graphics2D g2, int diameter)
+    {
+        /**
+         * draws the target
+         * @param g2 the grahpics content
+         */
+        
+        Ellipse2D black_circle = new Ellipse2D.Double(xLeft, yTop, diameter, diameter);
+        g2.draw(black_circle);
+        
+        
+    }
+}
