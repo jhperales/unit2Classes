4410405ad347d020576e95f9c5b42c1059320129
jhperales <jhperales@NN123-HA-18.HS.sd203.org>
2015-10-01 14:46:17 -0500

    update
    finished change class

--- Chapter 4 practice/Pseudocode_2.java
+++ Chapter 4 practice/Pseudocode_2.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 
+
 public class Pseudocode_2
 {
     public static void main(String [] args)
@@ -15,6 +16,33 @@ public class Pseudocode_2
         double change = recieved - due;
         System.out.printf("The change is: %.2f", change);
         
+        double dollar = change/1;
+        int dollars = (int) dollar;
+        change = change - dollars;
+        
+        double quarter = change/.25;
+        int quarters = (int) quarter;
+        change = change - (quarters * .25);
+        
+        double dime = change/.10;
+        int dimes = (int) dime;
+        change = change - (dimes * .10);
+        
+        double nickel = change/.05;
+        int nickels = (int) nickel;
+        change = change - (nickel * .05);
+        
+        double penny = change/.01;
+        int pennies = (int) penny;
+        change = change - (pennies * 100);
+        
+        
+        System.out.print("\n\nYou should give the following:");
+        System.out.print("\nDollars: " + dollars);
+        System.out.print("\nQuarters: " + quarters);
+        System.out.print("\nDimes: " + dimes);
+        System.out.print("\nNickels: " + nickels);
+        System.out.print("\nPennies: " + pennies);
         
     }
 }
