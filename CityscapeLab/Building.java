a209f94b9b1996ca0ea74142006ccd97c3dbea50
jhperales <jhperales@NN123-HA-18.HS.sd203.org>
2015-10-02 14:19:02 -0500

    sync

--- CityscapeLab/Building.java
+++ CityscapeLab/Building.java
@@ -1,40 +1,15 @@
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
+import java.awt.Color;
 
-
-/**
- * Write a description of class Building here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class Building
 {
-    /** description of instance variable x (add comment for each instance variable) */
-    private int x;
-
+    private int xValue;
+    private int yValue;
     /**
-     * Default constructor for objects of class Building
+     * a building that has can be placed anywhere
      */
-    public Building()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *    that describes the operation of the method
-     *
-     * @pre        preconditions for the method
-     *            (what the method assumes about the method's parameters and class's state)
-     * @post    postconditions for the method
-     *            (what the method guarantees upon completion)
-     * @param    y    description of parameter y
-     * @return    description of the return value
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x+y;
-    }
-
-}
+    public 
